9c24c80138cc829fe3edd5f047fe1818a4112eff
jason.jafari.m@gmail.com <jason.jafari.m@gmail.com>
2020-04-23 14:18:29 -0400

    ssss

--- p1.py
+++ p1.py
@@ -9,7 +9,7 @@ print("Matplotlib:" , matplotlib.__version__)
 ## feed forward Multy leyare perceptron
 inputs = [1.2,5.1,2.1]
 weights = [3.1,2.1,8.7]
-bias = 3
+bias = 31  
 
 #output = inputs[0]*weight
  
