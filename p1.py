50b547e16bfb9fc3492942a738dc4ab26332c86f
thisisjasonjafari <jason.jafari.m@gmail.com>
2020-04-23 14:14:39 -0400

    1212

---
+++ p1.py
@@ -0,0 +1,16 @@
+import sys
+import numpy as np
+import matplotlib
+
+print("Python:" , sys.version)
+print("Numpy:" , np.__version__)
+print("Matplotlib:" , matplotlib.__version__)
+
+## feed forward Multy leyare perceptron
+inputs = [1.2,5.1,2.1]
+weights = [3.1,2.1,8.7]
+bias = 3
+
+#output = inputs[0]*weight
+ 
+ 
